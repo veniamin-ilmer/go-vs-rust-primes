5a2c9a6471eeb71e1043bc46079e989fcb770afc
Veniamin Ilmer <39828997+veniamin-ilmer@users.noreply.github.com>
2018-06-25 09:08:03 -0500

    Create prime_expanding_memory.rs

---
+++ prime_expanding_memory.rs
@@ -0,0 +1,24 @@
+fn main() {
+
+  let mut vec_primes: Vec<u64> = Vec::with_capacity(20000000);
+  for i in 2..20000000 {
+    if vec_primes.iter().take_while(|&p| p * p <= i).all(|&p| i % p != 0) {
+      vec_primes.push(i);
+    }
+  }
+  println!("{}", vec_primes.len());
+
+}
+
+//Compiled: cargo build --release
+//6.0 sec
+//Memory Before: 564 K
+//Memory After: 10,512 K
+
+/* Pause code to measuring memory.
+use std::io;
+use std::io::prelude::*;
+
+let mut stdin = io::stdin();
+let _ = stdin.read(&mut [0u8]).unwrap();
+*/
