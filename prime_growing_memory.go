b94e8a131b89b01d8a4009023f9ff257a5f1d63a
Veniamin Ilmer <39828997+veniamin-ilmer@users.noreply.github.com>
2018-06-25 09:25:01 -0500

    Update and rename prime_expanding_memory.go to prime_growing_memory.go

--- prime_growing_memory.go
+++ prime_growing_memory.go
@@ -4,21 +4,21 @@ import "fmt"
 
 func main() {
 
-  var slice_primes []int;
+  var array_primes []int;
 
   for i := 2; i < 20000000; i++ {
     is_prime := true;
-    for j := 0; j < len(slice_primes) && slice_primes[j] * slice_primes[j] <= i; j++ {
-      if i % slice_primes[j] == 0 {
+    for j := 0; j < len(array_primes) && array_primes[j] * array_primes[j] <= i; j++ {
+      if i % array_primes[j] == 0 {
         is_prime = false;
         break;
       }
     }
     if is_prime {
-      slice_primes = append(slice_primes, i);
+      array_primes = append(array_primes, i);
     }
   }
-  fmt.Println(len(slice_primes));
+  fmt.Println(len(array_primes));
   bufio.NewReader(os.Stdin).ReadBytes('\n')
 
 }
